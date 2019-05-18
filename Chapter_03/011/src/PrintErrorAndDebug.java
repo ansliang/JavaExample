a0e1921233d3dccb96e0ec4586e5440482e8646d
ansLiang <chaohengliang@gmail.com>
2019-05-18 10:09:06 +0800

    输出错误信息与调试信息

---
+++ Chapter_03/011/src/PrintErrorAndDebug.java
@@ -0,0 +1,9 @@
+public class PrintErrorAndDebug {
+    public static void main(String[] args) {
+        System.out.println("main()方法开始运行了。");
+        System.err.println("在运行期间手动输出一个错误:");
+        System.err.println("请注意安全。");
+        System.out.println("PrintErrorAndDebug.main()");
+        System.out.println("main()方法运行结束。");
+    }
+}
