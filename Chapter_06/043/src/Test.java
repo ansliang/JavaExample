15477ee8a099029e8d009f0d731d54f917411ded
ansLiang <chaohengliang@gmail.com>
2019-05-19 16:34:46 +0800

    自定义图书类

---
+++ Chapter_06/043/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Book book = new Book("《Java核心技术卷一》", "Cay", 119.00);
+        System.out.println("书名：" + book.getTitle());
+        System.out.println("作者：" + book.getAuthor());
+        System.out.println("价格：" + book.getPrice());
+    }
+}
