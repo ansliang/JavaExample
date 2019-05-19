edf6e994b9e73a9e112bb4e411164877d6337002
ansLiang <chaohengliang@gmail.com>
2019-05-19 22:19:26 +0800

    Java对象的假克隆（使用“=”）

---
+++ Chapter_06/054/src/Employee.java
@@ -0,0 +1,25 @@
+public class Employee {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "姓名：" + name + ", 年龄：" + age;
+    }
+}
