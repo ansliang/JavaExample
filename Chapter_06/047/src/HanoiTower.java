c333da85345b8fea17e1322497c37df7e3db251b
ansLiang <chaohengliang@gmail.com>
2019-05-19 17:16:03 +0800

    汉诺塔问题求解（static递归）

---
+++ Chapter_06/047/src/HanoiTower.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HanoiTower {
+    public static void moveDish(int level, char from, char inter, char to) {
+        if (level == 1) {
+            System.out.println("从" + from + "移动盘子1号到" + to);
+        } else {
+            moveDish(level-1, from, to, inter);
+            System.out.println("从" + from + "移动盘子" + level + "号到" + to);
+            moveDish(level - 1, inter, from, to);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int nDisks = in.nextInt();
+        moveDish(nDisks, 'A', 'B', 'C');
+    }
+}
